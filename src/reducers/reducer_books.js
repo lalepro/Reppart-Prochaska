ffe8808b4eaa7df2822f184912387251a54d46ce
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-06-14 15:54:54 -0500

    rewriting reducer

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,12 +1,12 @@
 export default function(){
     return [
-        { title: 'History of Darby and Laura', summary: 'Two women fall in love', rating: '5 stars', pages: 101 },
-        { title: '2014', summary: 'We met in a bar', rating: '5 stars', pages: 101 },
-        { title: '2015', summary: 'We got to know eachother', rating: '5 stars', pages: 89  },
-        { title: '2016', summary: 'We traveled a lot', rating: '4 stars', pages: 78  },
-        { title: '2017', summary: 'We moved to Texas!', rating: '5 stars', pages: 205  },
-        { title: '2018', summary: 'We bought a house and we got engaged!', rating: '5 stars', pages: 205  },
-        { title: '2019', summary: 'We planned a wedding!', rating: '5 stars', pages: 205  },
-        { title: '2020', summary: 'We got married!', rating: '5 stars', pages: 205  }
+        { title: 'History of Darby and Laura', summary: 'Two women fall in love', rating: '5 stars', details: 101 },
+        { title: '2014', summary: 'We met in a bar', rating: '5 stars', details: 101 },
+        { title: '2015', summary: 'We got to know eachother', rating: '5 stars', details: 89  },
+        { title: '2016', summary: 'We traveled a lot', rating: '4 stars', details: 78  },
+        { title: '2017', summary: 'We moved to Texas!', rating: '5 stars', details: 205  },
+        { title: '2018', summary: 'We bought a house and we got engaged!', rating: '5 stars', details: 205  },
+        { title: '2019', summary: 'We planned a wedding!', rating: '5 stars', details: 205  },
+        { title: '2020', summary: 'We got married!', rating: '5 stars', details: 205  }
     ];
 }
