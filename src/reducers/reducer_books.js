78e923e64d5572a701f784c6c1f71670ddc89a44
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-06-14 16:04:00 -0500

    save changes

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,10 +1,10 @@
 export default function(){
     return [
         { title: 'History of Darby and Laura', summary: 'Two women fall in love', rating: '5 stars', details: 'Two small town girls, looking for love in a big city' },
-        { title: '2014', summary: 'We met in a bar', rating: '5 stars', details: 'August 16, 2014: both independantly out on town, eyes meet in a bar, both smile, the rest in history.' },
-        { title: '2015', summary: 'We got to know eachother', rating: '5 stars', details: 'Learning how to adjust life to hold a new person, fun and hard all at the same time'  },
+        { title: '2014', summary: 'We met in a bar', rating: '4 stars', details: 'August 16, 2014: both independantly out on town, eyes meet in a bar, both smile, the rest in history.' },
+        { title: '2015', summary: 'We got to know eachother', rating: '4 stars', details: 'Learning how to adjust life to hold a new person, fun and hard all at the same time'  },
         { title: '2016', summary: 'We traveled a lot', rating: '4 stars', details: 'Best way to live life? Travel: From Spain to Martineque, Canada, Out West, and Key West'  },
-        { title: '2017', summary: 'We moved to Texas!', rating: '5 stars', details: 'When Traveling is just the start to actually living somewhere new.  },
+        { title: '2017', summary: 'We moved to Texas!', rating: '5 stars', details: 'When Traveling is just the start to actually living somewhere new.'  },
         { title: '2018', summary: 'We bought a house and we got engaged!', rating: '5 stars', details: 'Own land in Texas and rings on figures!'  },
         { title: '2019', summary: 'We planned a wedding!', rating: '5 stars', details: 'tbd'  },
         { title: '2020', summary: 'We got married!', rating: '5 stars', details: 'tbd'  }
